336312b401a129be0c59563bb7ab67809a424c0d
iammatthewmb <matrix.brown@gmail.com>
2019-02-13 22:22:17 +0200

    updated addition

--- src/test/java/ac/za/cput/AppTest.java
+++ src/test/java/ac/za/cput/AppTest.java
@@ -1,5 +1,8 @@
 package ac.za.cput;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import org.junit.Test;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -7,14 +10,16 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        App app = new App();
+        assertEquals(2, app.Addition(1, 1));
+
+
     }
 }

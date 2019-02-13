22924d0cb78af34e913ba603a95c1a0671c499f9
iammatthewmb <matrix.brown@gmail.com>
2019-02-13 14:09:15 +0200

    added code

--- src/main/java/ac/za/cput/App.java
+++ src/main/java/ac/za/cput/App.java
@@ -1,13 +1,42 @@
 package ac.za.cput;
-
+import java.util.*;
 /**
  * Hello world!
  *
  */
 public class App 
+
 {
+
     public static void main( String[] args )
     {
+        Scanner scan = new Scanner(System.in);
+        int num1;
+        int num2;
+
+
         System.out.println( "Hello World!" );
+
+
+
+        System.out.println("Enter num1 ");
+        num1 = scan.nextInt();
+        System.out.println("Enter num25 ");
+        num2 = scan.nextInt();
+        int result;
+        result = num1 + num2;
+
+        //result = scan.nextInt();
+
+
+        System.out.println("Result: " + result);
+
+
+
+
     }
+
+
+
+
 }
